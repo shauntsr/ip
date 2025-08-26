94d8cd217583318a683e2549575238e71cdb1424
Shaun Tan <shauntansr@gmail.com>
2025-08-26 17:55:10 +0800

    add increment: level-0

---
+++ src/main/java/Odin.java
@@ -0,0 +1,13 @@
+public class Odin {
+    public static void printHorizontalLine(){
+        System.out.println("__________________________________________");
+    }
+    public static void main(String[] args) {
+        printHorizontalLine();
+        System.out.println("Hello! I'm Odin");
+        System.out.println("What can I do for you?");
+        printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
+    }
+}
