f739152ddce36d8121f015812a7d301d3d3a5843
Shaun Tan <shauntansr@gmail.com>
2025-09-01 18:28:17 +0800

    Added TaskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,15 @@
+public class TaskList {
+    private Task[] taskList = new Task[100];
+    private int taskCount = 0;
+
+    public void addTask(String description) {
+        taskList[taskCount] = new Task(description);
+        taskCount++;
+    }
+
+    public void printTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println(taskList[i].getDescription());
+        }
+    }
+}
