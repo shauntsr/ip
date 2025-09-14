707b7ea0524843d88cbd6c0d000d7db032d36064
Shaun Tan <shauntansr@gmail.com>
2025-09-14 17:38:19 +0800

    Refactor user input handling.

--- src/main/java/Odin.java
+++ src/main/java/Odin.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 public class Odin {
-    public static void printHorizontalLine(){
+    public static void printHorizontalLine() {
         System.out.println("__________________________________________");
     }
+
     public static void main(String[] args) {
         TaskList t = new TaskList();
 
@@ -18,27 +19,36 @@ public class Odin {
             String input = scanner.nextLine();
 
             printHorizontalLine();
-
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                printHorizontalLine();
-                return;
-            }
-            if (input.equals("list")) {
-                System.out.println("These are your tasks.");
-                t.printTasks();
-            } else if (input.startsWith("mark")) {
-                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                t.markTask(taskIndex);
-            } else if (input.startsWith("unmark")) {
-                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                t.unmarkTask(taskIndex);
-            } else {
-                t.addTask(input);
-                System.out.println("added: " + input);
-            }
-
+            if (handleInput(input, t)) return;
             printHorizontalLine();
         }
-   }
+    }
+
+    public static boolean handleInput(String input, TaskList t) {
+        String command = input.split(" ")[0];
+
+        switch (command) {
+        case "bye":
+            System.out.println("Bye. Hope to see you again soon!");
+            printHorizontalLine();
+            return true;
+        case "list":
+            System.out.println("These are your tasks.");
+            t.printTasks();
+            break;
+        case "mark":
+            int markIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+            t.markTask(markIndex);
+            break;
+        case "unmark":
+            int unmarkIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+            t.unmarkTask(unmarkIndex);
+            break;
+        default:
+            t.addTask(input);
+            System.out.println("added: " + input);
+        }
+        return false;
+    }
+
 }
