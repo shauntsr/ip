e6ae5b01eb94bf9dca5398da6b9a4fafdd611d83
Shaun Tan <shauntansr@gmail.com>
2025-09-24 22:42:56 +0800

    Add method for Tasks to convert to file string.

--- src/main/java/odin/task/ToDo.java
+++ src/main/java/odin/task/ToDo.java
@@ -9,4 +9,9 @@ public class ToDo extends Task {
     public String toString() {
         return "[T]" + super.toString();
     }
+
+    @Override
+    public String toFileString() {
+        return "T" + DELIM + super.toFileString();
+    }
 }
