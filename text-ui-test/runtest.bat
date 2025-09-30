835cd159a9db1522f009a697d2db6b577e373ce5
Shaun Tan <shauntansr@gmail.com>
2025-10-01 00:52:44 +0800

    Update test cases and EXPECTED.TXT, and add deleting of data folder to runtest.bat

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,6 +6,8 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
+REM delete data folder from previous run
+if exist data rmdir /s /q data
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\odin\Odin.java
 IF ERRORLEVEL 1 (
