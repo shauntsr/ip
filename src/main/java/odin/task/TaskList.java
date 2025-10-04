7a0df87591350680ee6bdfed064e46558cf4eabc
Shaun Tan <shauntansr@gmail.com>
2025-10-04 15:47:05 +0800

    Add docs for TaskList.

--- src/main/java/odin/task/TaskList.java
+++ src/main/java/odin/task/TaskList.java
@@ -2,13 +2,29 @@ package odin.task;
 
 import java.util.ArrayList;
 
+/**
+ * The {@code TaskList} class represents a collection of {@link Task} objects.
+ * It provides methods for adding, removing, marking, unmarking, finding,
+ * and retrieving tasks.
+ */
 public class TaskList {
     private ArrayList<Task> taskList = new ArrayList<>();
 
+    /**
+     * Adds a new {@link Task} to the task list.
+     *
+     * @param task The task to be added.
+     */
     public void addTask(Task task) {
         taskList.add(task);
     }
 
+    /**
+     * Marks a task by setting as done.
+     *
+     * @param taskIndex The index of the task to mark.
+     * @throws IndexOutOfBoundsException If the index is invalid.
+     */
     public void markTask(int taskIndex) {
         if (taskIndex < 0 || taskIndex >= taskList.size()) {
             throw new IndexOutOfBoundsException();
@@ -16,6 +32,12 @@ public class TaskList {
         taskList.get(taskIndex).setIsDone(true);
     }
 
+    /**
+     * Unmarks a task by setting as not done.
+     *
+     * @param taskIndex The index of the task to unmark.
+     * @throws IndexOutOfBoundsException If the index is invalid.
+     */
     public void unmarkTask(int taskIndex) {
         if (taskIndex < 0 || taskIndex >= taskList.size()) {
             throw new IndexOutOfBoundsException();
@@ -23,6 +45,12 @@ public class TaskList {
         taskList.get(taskIndex).setIsDone(false);
     }
 
+    /**
+     * Deletes a task from the task list.
+     *
+     * @param taskIndex The index of the task to delete.
+     * @throws IndexOutOfBoundsException If the index is invalid.
+     */
     public void deleteTask(int taskIndex) {
         if (taskIndex < 0 || taskIndex >= taskList.size()) {
             throw new IndexOutOfBoundsException();
@@ -30,6 +58,12 @@ public class TaskList {
         taskList.remove(taskIndex);
     }
 
+    /**
+     * Finds all tasks in the list whose description contains the given query (case-insensitive).
+     *
+     * @param query The search query string.
+     * @return A list of indices representing the positions of matching tasks.
+     */
     public ArrayList<Integer> findTask(String query) {
         ArrayList<Integer> taskIndices = new ArrayList<Integer>();
 
@@ -42,10 +76,22 @@ public class TaskList {
         return taskIndices;
     }
 
+    /**
+     * Returns the total number of tasks in the list.
+     *
+     * @return The number of tasks.
+     */
     public int getTaskCount() {
         return taskList.size();
     }
 
+    /**
+     * Retrieves a task at a given index.
+     *
+     * @param index The index of the task to retrieve.
+     * @return The {@link Task} at the specified index.
+     * @throws IndexOutOfBoundsException If the index is invalid.
+     */
     public Task getTask(int index) {
         return taskList.get(index);
     }
